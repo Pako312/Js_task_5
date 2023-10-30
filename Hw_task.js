41ba74bb50576224e786b223d0a59c9d377f9f48
Timur <tima97855@gmail.com>
2023-10-30 07:57:06 +0600

    Task_5

--- Hw_task.js
+++ Hw_task.js
@@ -51,3 +51,46 @@
 // alert(result);
 
 // 5) Сделайте функцию, которая принимает параметром число от 1 до 7, а возвращает день недели на русском языке.
+
+
+function getDayOfWeek(number) {
+   
+
+    switch (number) {
+    
+    case 1:
+    
+    return "Понедельник";
+    
+    case 2:
+    
+    return "Вторник";
+    
+    case 3:
+    
+    return "Среда";
+    
+    case 4:
+    
+    return "Четверг";
+    
+    case 5:
+    
+    return "Пятница";
+    
+    case 6:
+    
+    return "Суббота";
+    
+    case 7:
+    
+    return "Воскресенье";
+    
+    default:
+    
+    return "Некорректный день недели";
+    
+    }
+    }
+    result = getDayOfWeek(7);
+   alert(result);
